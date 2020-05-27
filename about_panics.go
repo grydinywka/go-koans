e60304b795c0405b622c58b25418200aa44515d0
Sergiy Ignatenko <grydinywka@gmail.com>
2020-05-27 15:05:22 +0300

    Fix about_panics.go

--- about_panics.go
+++ about_panics.go
@@ -5,10 +5,18 @@ func divideFourBy(i int) int {
 }
 
 const __divisor__ = 0
+const __properDivisor__ = 2
 
 func aboutPanics() {
-	assert(__delete_me__) // panics are exceptional errors at runtime
-
+	//assert(__delete_me__) // panics are exceptional errors at runtime
+	defer func() {
+		if r := recover(); r != nil {
+			if  err, ok := r.(error); ok && err != nil {
+				n := divideFourBy(__properDivisor__)
+				assert(n == 2)
+			}
+		}
+	}()
 	n := divideFourBy(__divisor__)
 	assert(n == 2) // panics are exceptional errors at runtime
 }
